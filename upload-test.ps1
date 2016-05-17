6c96a9c3cfb10f6bbfa4f87049ff8948ef1980d6
Feodor Fitsner <feodor@appveyor.com>
2016-05-17 12:52:01 -0700

    Create upload-test.ps1

---
+++ upload-test.ps1
@@ -0,0 +1,33 @@
+$token = $env:gh_token
+$uploadFilePath = "$env:appveyor_build_folder\node.zip"
+$releaseName = "v$env:appveyor_build_version"
+
+$headers = @{
+  "Authorization" = "token $token"
+  "Content-type" = "application/json"
+}
+
+$body = @{
+  tag_name = $releaseName
+  target_commitish = "master"
+  name = $releaseName
+  body = "Description of the release"
+  draft = $false
+  prerelease = $false
+}
+
+Write-Host "Creating release..." -NoNewline
+$json = (ConvertTo-Json $body)
+$release = Invoke-RestMethod -Uri 'https://api.github.com/repos/FeodorFitsner/aaa1/releases' -Headers $headers -Method POST -Body $json
+$uploadUrl = $release.upload_url.Replace("{?name,label}", "") + "?name=" + [IO.Path]::GetFileName($uploadFilePath)
+Write-Host "OK" -ForegroundColor Green
+
+$uploadUrl
+
+Write-Host "Uploading asset..." -NoNewline
+$data = [System.IO.File]::ReadAllBytes($uploadFilePath)
+$wc = New-Object Net.WebClient
+$wc.Headers['Content-type'] = 'application/octet-stream'
+$wc.Headers['Authorization'] = "token $token"
+$response = $wc.UploadData($uploadUrl, "POST", $data)
+Write-Host "OK" -ForegroundColor Green
